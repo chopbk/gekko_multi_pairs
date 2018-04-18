5e4c2e3410e048040649eaca66061d2b9d613d9c
tsuperprince <dinhtam94@gmail.com>
2018-04-18 14:22:20 +0000

    modified:   configBTC_BNB.js 	modified:   run/runBTC_BNB.sh 	modified:   run/runBTC_ETH.sh 	configBTC_ADA.js 	configBTC_TNB.js 	run/runBTC_TNB.sh

--- configBTC_BNB.js
+++ configBTC_BNB.js
@@ -15,8 +15,8 @@ config.watch = {
   currency:'BTC',
   asset: 'BNB',
   enable_fix_amount: true,
-  max_amount_currency_buy: 0.0015,
-  max_amount_asset_sell: 0,
+  max_amount_currency_buy: 0,
+  max_amount_asset_sell: 1.5,
   // You can set your own tickrate (refresh rate).
   // If you don't set it, the defaults are 2 sec for
   // okcoin and 20 sec for all other exchanges.
